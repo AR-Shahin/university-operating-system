0e5df2c1ca282b6777fec2ab3f2dbab9e92e695b
AR-Shahin <mdshahinmije96@gmail.com>
2023-09-20 20:09:47 +0600

    loop and conditions

---
+++ condtions/ars.bash
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+a=50
+b=3
+
+# if [ $a -eq $b ] ;
+# then
+#     echo "equal"
+
+# elif [ $a -gt $b ] ;
+# then
+#     echo "A is big"
+
+# elif [ $a -lt $b ] ;
+# then
+#     echo "A is small"
+
+# else
+#     echo "not equal"
+# fi
+
+# even or odd
+# if [ $(( $a % 2 )) -eq 0 ]
+# then
+#     echo "Even"
+# else
+#     echo "Odd"
+# fi
+
+
+# loop 
+# sum=1
+# for i in {1..5}
+# do
+#     # if [ $(( $i % 2)) -eq 0 ]
+#     # then
+#     #     echo "Even -> $i"
+#     # else
+#     #     echo "Odd -> $i"
+#     sum=$(($sum * $i))
+#     # fi
+# done
+
+# echo $sum
+
+# for ((i=1; i<=10; i+=2))  
+# do
+#     echo $i
+# done
+
---
+++ condtions/main.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Conditions
+
+# if else
+# let a=30
+# let b=10
+# string decrale er jnno let hbe na 
+a="a"
+b="qhii"
+
+# substring 
+# if [[ $b == *i* ]]
+# then
+#     echo "yes"
+# else
+#     echo " no"
+# fi
+
+# empty string check 
+# a=""
+# if [ -z $a ]
+# then
+#     echo "yes"
+# else
+#     echo " no"
+# fi
+
+
+# Loop 
+
+# for i in {0..4}
+# do
+#     echo "i = $i"
+# done
+
+# for i in 1 4 4 5
+# do
+#     echo "i = $i"
+# done
+
+
+# for i in {0..4..2}
+# do
+#     echo "i = $i"
+# done
+
+
+# for ((i=0; i<5; i+=2))
+# do
+#     echo "i = $i"
+# done
+
+# While loop
+
+# while [Conditions]
+# do 
+#     # code
+# done
+# let a=10
+# while [ $a -ge 1 ]
+# do 
+#     echo "$a"
+#     a=$(( a-1 ))
+# done
+
+
+# funcitons 
+
+hello( ){
+    return 5
+}
+
+
+world(){
+    
+    
+    
+
+}
+
+world
